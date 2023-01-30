714c6392e477da9dbcf2aed34016e604b6ee2605
Adamay Bhardwaj <92214282+AdamayB@users.noreply.github.com>
2023-01-30 21:33:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import pikepdf
-path="C:\\Users\\adama\\Documents\\Thapar_Study_material\\Signals and Systems\\"
-def removePass(a,b):
-    removePass = pikepdf.open(a, password="2021!")
+path="" #enter path
+def removePass(a,b,password):
+    removePass = pikepdf.open(a, password)
     removePass.save(b)
     print('Unlocked')
-l=['09.2',10.1,10.2]
-for i in l:
-    path1=path+'S&S_UEC_LECT'+str(i)+'.pdf'
-    path2=path+'S&S_UEC_LECT'+str(i)+'(unlocked).pdf'
-    removePass(path1,path2)
-
-'''removePass=pikepdf.open(path1,password="2021!")
-removePass.save(path+'S&S_UEC_LECT01(unlocked).pdf')
-print('Unlocked')'''
+# a= the original file name
+# b= the unlocked file name
+#password =password of the original file
